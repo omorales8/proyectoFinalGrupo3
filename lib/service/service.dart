766dd3e3740b49d407ae3c531a98d31602db2ed0
oswaldo <omoralesm8@miumg.edu.gt>
2022-05-31 00:07:12 -0600

    1212
    jhufytc

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -6,116 +6,95 @@ import 'package:note/model/note.dart';
 import 'dart:convert';
 
 class Service {
-
   static Future<List<Note>> getNotes() async {
     var url = Uri.parse('${Environment.apiUrl}');
 
     List<Note> listNote = [];
-    try { 
-      var response = await http.get(url,headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        });
+    try {
+      var response = await http.get(url, headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      });
 
-      var body = response.body; 
-
-      if(response.statusCode >=  400) {
-        return listNote;
-      }
-      if(response.statusCode ==  200) {
-        listNote = noteFromMap(body);
-
-        return listNote;
-      }          
-    } catch (e) { 
-        return listNote;
+      var body = response.body;
+      listNote = noteFromMap(body);
+    } catch (e) {
+      return listNote;
     }
-      return listNote; 
+    return listNote;
   }
 
-
-
-  
-  static Future<Note> createNote(Note note)async {
+  static Future<Note> createNote(Note note) async {
     var url = Uri.parse('${Environment.apiUrl}');
     Map<String, String> qParams = {
-    'isImportant': note.isImportant.toString(),  
-    'tittle': note.title,
-    'description': note.description,
+      'isImportant': note.isImportant.toString(),
+      'tittle': note.title,
+      'description': note.description,
     };
 
     Map<String, String> header = {
-    HttpHeaders.contentTypeHeader: "application/json"
+      HttpHeaders.contentTypeHeader: "application/json"
     };
 
-    final finalUri = url.replace(queryParameters: qParams); 
+    final finalUri = url.replace(queryParameters: qParams);
 
-    try { 
+    try {
       final response = await http.put(
         finalUri,
         headers: header,
       );
 
-
-      if(response.statusCode >=  400) {
+      if (response.statusCode >= 400) {
         return note;
       }
-      if(response.statusCode ==  200) {
-
-
+      if (response.statusCode == 200) {
         return note;
-      }          
-    } catch (e) { 
+      }
+    } catch (e) {
       throw Exception(e);
     }
-      return note; 
+    return note;
   }
 
-
-  static Future<int> deleteNote(int id)async {
+  static Future<int> deleteNote(int id) async {
     var url = Uri.parse('${Environment.apiUrl}');
-    Map<String, String> qParams = {
-    'id': id.toString()
-    };
+    Map<String, String> qParams = {'id': id.toString()};
 
     Map<String, String> header = {
-    HttpHeaders.contentTypeHeader: "application/json"
+      HttpHeaders.contentTypeHeader: "application/json"
     };
 
-    final finalUri = url.replace(queryParameters: qParams); 
+    final finalUri = url.replace(queryParameters: qParams);
     var status;
-    try { 
+    try {
       final response = await http.delete(
         finalUri,
         headers: header,
       );
-      status = response.statusCode;         
-    } catch (e) { 
+      status = response.statusCode;
+    } catch (e) {
       throw Exception(e);
     }
-      return status; 
+    return status;
   }
 
-
-  static Future<Note> getNote(int id)async {
+  static Future<Note> getNote(int id) async {
     var url = Uri.parse('${Environment.apiUrl}/${id}');
 
-    Note note; 
-    try { 
-       var response = await http.get(url,headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        });
-      var body = response.body; 
-      print(body);      
-
+    Note note;
+    try {
+      var response = await http.get(url, headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      });
+      var body = response.body;
+      print(body);
 
       Map<String, dynamic> noteObjet = jsonDecode(body);
       note = Note.fromJson(noteObjet);
-             
-    } catch (e) { 
+    } catch (e) {
       throw Exception(e);
     }
-      return note; 
+    return note;
   }
 }
